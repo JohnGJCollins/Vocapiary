39d546e1088dd10567f8a1916a8b163ac941708f
John Collins <fakeemail>
2023-01-06 15:42:04 -0500

    Added in elements for login, planning login implmt

--- js/production.js
+++ js/production.js
@@ -1,3 +1,11 @@
 /*
 This is where I plan to actually implement the vocabulary learning feature.
 */
+
+function signin() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    document.getElementById("accountinfo").textContent = "Account: " + username; //Doesn't actually let you sign in yet, lol
+    console.log(username);
+}
